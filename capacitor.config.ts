79ddd9423c723e4fbae1146ba262c1e9125e09e9
Boukada Adel <65848859+Leknif571@users.noreply.github.com>
2026-05-09 22:40:24 +0200

    fix : add ionic and capacitor to make cross-plateform app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hubertsocity.hubertapp',
+  appName: 'hubertapp',
+  webDir: 'dist'
+};
+
+export default config;
